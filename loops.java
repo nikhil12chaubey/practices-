7b7e9d66e61cf34fd56155ee4918d8e0ab647ce6
nikhil12chaubey <nikhilchaubey082@gmail.com>
2026-09-01 15:56:42 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,11 @@
+public class loops {
+    public static void main(String[] args) {
+        boolean isup = false;
+        if (isup == true) {
+            System.out.println("morning");
+        } else
+            System.out.println("evening");
+
+    }
+
+}
---
+++ price.java
@@ -0,0 +1,12 @@
+public class price {
+    public static void main(String[] args) {
+        double price = 100.00;
+        double finalprice = price + 18;
+        System.out.println(finalprice);
+
+        int p = 10;
+        int ft = 10.0;
+        int pf = p + ft;
+        System.out.println(pf);
+    }
+}
